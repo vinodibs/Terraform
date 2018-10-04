7e0f81313341de4d83038c4e2a0e4290cb959658
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-10-04 23:15:40 +0530

    Update variables.tf

---
+++ ec2-instance/variables.tf
@@ -0,0 +1,53 @@
+variable "region" {
+  description = "Provide here region name"
+  type        = "string"
+  default     = ""
+}
+
+variable "ami" {
+  description = "Your Ami id "
+  type        = "string"
+  default     = ""
+}
+
+variable "instance_type" {
+  description = "Type of instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "key_name"
+  default	  = "streo"
+}
+
+variable "disable_api_termination" {
+  description = "disable_api_termination"
+  default     = "true"
+}
+
+variable "subnet_id" {
+  description = "subnet_id"
+  default     = ""
+}
+
+variable "vpc_security_group_ids" {
+  description = "vpc_security_group_ids"
+  type        = "string"
+  default     = ""
+}
+
+variable "count" {
+  description = "count"
+  default	  = "1"
+}
+
+variable "volume_tags" {
+  description = "volume_tags"
+  default     = "vinod"
+}
+
+variable "volume_size" {
+  description = "volume_size"
+  type        = "string"
+  default     = ""
+}
