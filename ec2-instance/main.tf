34947c701c54ad9e58a8d7d9a779bb7943e47d05
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-10-04 22:58:07 +0530

    Update main.tf

---
+++ ec2-instance/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+# Public Ec2-Instance
+resource "aws_instance" "Public_Ec2" {
+  ami                                  = "${var.ami}  # Ubuntu 16.04 "ami-188fba77"
+  instance_type                        = "${var.instance_type}"
+  instance_initiated_shutdown_behavior = "stop"
+  key_name                             = ${var.key_name}"
+  disable_api_termination              = ${var.disable_api_termination}"
+  subnet_id                            = "${var.subnet_id}"
+  associate_public_ip_address          = "${associate_public_ip_address}"
+  vpc_security_group_ids               = ["${var.vpc_security_group_ids}"]
+  ebs_optimized                        = false
+  monitoring                           = true
+  count                                = ${var.count}"
+
+# Root EBS Volume Size
+root_block_device {
+volume_tags 	= "${var.volume_tags}"
+volume_type 	= "gp2"
+volume_size		= ${var.volume_size}"
+}
+
+  tags {
+    Name = "Public-Ec2-Ubuntu-16.04"
+  }
+  
+}
