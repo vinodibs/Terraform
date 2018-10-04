f7aa5a2514fc1c4029900a8ae77e37dc33a1f477
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-10-04 23:48:43 +0530

    Update main.tf

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -4,19 +4,19 @@ provider "aws" {
 
 # Public Ec2-Instance
 resource "aws_instance" "Public_Ec2" {
-  ami                                  = "${var.ami}"  # Ubuntu 16.04 "ami-188fba77"
+  ami                                  = "${var.ami}"
   instance_type                        = "${var.instance_type}"
   instance_initiated_shutdown_behavior = "stop"
   key_name                             = "${var.key_name}"
   disable_api_termination              = "${var.disable_api_termination}"
-  subnet_id                            = "${var.subnet_id}"
+  #subnet_id                            = "${var.subnet_id}"
   associate_public_ip_address          = "${var.associate_public_ip_address}"
   vpc_security_group_ids               = ["${var.vpc_security_group_ids}"]
   ebs_optimized                        = "false"
   monitoring                           = "true"
   count                                = "${var.count}"
  
-# Root EBS Volume Size
+# Volume root_ebs size
 root_block_device {
 volume_type 	= "gp2"
 volume_size		= "${var.volume_size}"
