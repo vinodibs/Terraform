4b3d5eff751c7fb7fc2d51403815e9dd212b8c99
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-10-10 22:11:20 +0530

    Update main.tf

--- ec2-instance/examples/main.tf
+++ ec2-instance/examples/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "ap-south-1"
 }
 
 module "vpc" {
@@ -9,9 +9,9 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   assign_generated_ipv6_cidr_block = true
 
